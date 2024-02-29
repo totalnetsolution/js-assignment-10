f85c357e12cd021057b0395145641274e8d51c75
muhammad-danish <totalnetsolution2@gmail.com>
2024-03-01 04:26:41 +0500

    index file created

---
+++ app.js
@@ -0,0 +1,23 @@
+document.write("<h2>Rules for naming JS variables</h2><br><br> \n Variable names can only contain, numbers, $ and _. <br> For example : $my_1stVariable\nVariable must begin with a letter, $ or _ . <br> For example : $name, _name or name. <br>");
+document.write("Variable name are case sensitive. <br> Variable names should not be JS keywords.");
+
+
+var number1=5;
+var number2=10;
+var sum=number1+number2;
+console.log(sum);
+
+var number1=5;
+var number2=10;
+
+var sum=number1+number2;
+console.log(sum);
+
+var multiplication=number1*number2;
+console.log(multiplication);
+
+var division =number1/number2;
+console.log(division);
+
+var modulus =number1%number2;
+console.log(modulus);
